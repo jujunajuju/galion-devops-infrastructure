733d5f65cbf05edf1b39113257317adf5e748b73
jujunajuju <galionpatrick242@gmail.com>
2026-07-27 02:47:23 +0000

    Enable S3 access logging

--- modules/aws/s3/main.tf
+++ modules/aws/s3/main.tf
@@ -8,3 +8,26 @@ resource "aws_s3_bucket" "bucket" {
   }
 
 }
+
+
+resource "aws_s3_bucket" "logs" {
+
+  bucket = "${var.project_name}-${var.environment}-logs"
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-logs"
+    Environment = var.environment
+  }
+
+}
+
+
+resource "aws_s3_bucket_logging" "bucket_logging" {
+
+  bucket = aws_s3_bucket.bucket.id
+
+  target_bucket = aws_s3_bucket.logs.id
+
+  target_prefix = "access-logs/"
+
+}
