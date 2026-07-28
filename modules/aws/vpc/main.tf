afb12b3ca95ceb40c10b002abf6c3ef3685bb3fe
jujunajuju <galionpatrick242@gmail.com>
2026-07-28 15:32:09 +0000

    Increase CloudWatch log retention to 365 days

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -31,7 +31,7 @@ resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
 
   name = "/aws/vpc/${var.environment}/flowlogs"
 
-  retention_in_days = 30
+  retention_in_days = 365
 
   kms_key_id = aws_kms_key.cloudwatch_logs.arn
 
