081157d859be33c2ce90eee19001d15224ff24ff
jujunajuju <galionpatrick242@gmail.com>
2026-07-28 21:26:25 +0000

    Fix S3 public access block security checks

--- modules/aws/s3/main.tf
+++ modules/aws/s3/main.tf
@@ -21,6 +21,16 @@ resource "aws_s3_bucket" "logs" {
 
 }
 
+resource "aws_s3_bucket_public_access_block" "logs_public_access_block" {
+
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+}
 
 resource "aws_s3_bucket_logging" "bucket_logging" {
 
